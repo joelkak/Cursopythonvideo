645f20aa7701dff767c90c96f6343ce5125682b5
joelkak <joe_turbulencia@hotmail.com>
2022-09-06 15:23:54 -0300

    tentando sempre

---
+++ aula 15 Interrompendo repetições while 'break'.py
@@ -0,0 +1,3 @@
+#break# interrompe o caomando do dev
+
+file:///var/home/joelsilverio/Imagens/Capturas%20de%20tela/Captura%20de%20tela%20de%202022-09-05%2011-11-01.png
---
+++ ex065 Maior e menor valores....py
@@ -0,0 +1,16 @@
+resp = 'S'
+soma = quant = média = maior = menor = 0
+while resp in 'Ss':
+    núm = int(input('Digite um número : '))
+    soma += núm
+    if quant == 1:
+        maior = menor = núm
+    else:
+        if núm > maior:
+            maior = núm
+        if núm < menor:
+            menor = núm
+    resp = str(input('Quer continuar? [S/N]')) .upper().strip()[0]
+média = soma/quant
+print('Você digitou {} números e a média foi {} '.format(quant , média))
+print('O maior valor foi {} e o menor foi {}'.format(maior , menor))
